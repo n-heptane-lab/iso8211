2dfc750031bfec834199083c9247db7469cb490a
Jeremy Shaw <jeremy@n-heptane.com>
2017-12-28 11:17:36 -0600

    can parse data descriptive fields now

--- Data/S57/ASCII.hs
+++ Data/S57/ASCII.hs
@@ -1,10 +1,12 @@
+{-# language OverloadedStrings #-}
 module Data.S57.ASCII where
 
-import Control.Applicative (liftA2, liftA3)
+import Control.Applicative ((<|>), liftA2, liftA3, optional)
 import Data.ByteString (ByteString)
 import Data.Attoparsec.ByteString.Char8 (Parser)
 import qualified Data.Attoparsec.ByteString.Char8 as A
 
+-- http://www.dtic.mil/dtic/tr/fulltext/u2/a281594.pdf
 -- https://github.com/freekvw/iso8211/blob/master/iso8211.py
 
 {-
@@ -102,6 +104,12 @@ c2i '7' = 7
 c2i '8' = 8
 c2i '9' = 9
 
+implicit2i :: [Char] -> Int
+implicit2i cs = implicit2i' 0 cs
+  where
+    implicit2i' acc [] = acc
+    implicit2i' acc (c:cs) = implicit2i' ((acc * 10) + (c2i c)) cs
+
 pDirectoryEntry :: EntryMap -> Parser DirectoryEntry
 pDirectoryEntry (EntryMap sfl sfp _ sft) =
   do ft <- A.take $ c2i sft
@@ -109,7 +117,7 @@ pDirectoryEntry (EntryMap sfl sfp _ sft) =
      fp <- A.take $ c2i sfp
      pure $ DirectoryEntry ft fl fp
 
-data Directory = Directory [DirectoryEntry]
+data Directory = Directory { _directoryEntries :: [DirectoryEntry] }
   deriving Show
 
 pDirectory :: EntryMap -> Parser Directory
@@ -117,11 +125,14 @@ pDirectory entryMap =
   do des <- A.manyTill (pDirectoryEntry entryMap) ft
      pure $ Directory des
 
+
+-- | unit terminator - Δ
 ut :: Parser ()
 ut =
   do A.char '\x1f'
      pure ()
 
+-- | field terminator - ∇
 ft :: Parser ()
 ft =
   do A.char '\x1e'
@@ -133,9 +144,201 @@ pNull =
      A.char '\0'
      pure ()
 
+pFieldName :: Parser ByteString
+pFieldName = A.takeTill ((==) '\x1f') <* ut
+
+data TruncatedEscapeSequence
+  = LexicalLevel0
+  | LexicalLevel1
+  | LexicalLevel2
+  deriving Show
+
+pTruncatedEscapeSequence :: Parser TruncatedEscapeSequence
+pTruncatedEscapeSequence =
+  A.choice [ A.string "   " *> pure LexicalLevel0
+           , A.string "-A " *> pure LexicalLevel1
+           , A.string "%/A" *> pure LexicalLevel2
+           ]
+
+data FieldControlField = FieldControlField
+  { _fieldTags :: [(ByteString, ByteString)]
+  }
+  deriving Show
+
+pFieldPair :: Parser (ByteString, ByteString)
+pFieldPair = (,) <$> pFieldTag <*> pFieldTag
+
+pFieldControlFieldTag :: Parser ()
+pFieldControlFieldTag =
+  A.string "0000" *> pure ()
+
+pFieldControlFieldBody :: Parser FieldControlField
+pFieldControlFieldBody =
+  do pPrintableGraphics
+     _tes <- pTruncatedEscapeSequence
+     ut
+     pairs <- A.manyTill pFieldPair ft
+     pure $ FieldControlField pairs
+
+pFieldControlField :: Parser FieldControlField
+pFieldControlField =
+  pFieldControlFieldTag *> pFieldControlFieldBody
+
+-- | parse a 4 character field tag
+--
+-- No validation done, takes any 4 chars
+pFieldTag :: Parser ByteString
+pFieldTag = A.take 4
+
+pPrintableGraphics :: Parser ()
+pPrintableGraphics =
+  A.string ";&" *> pure ()
+
+data DataStructureCode
+  = Singleton -- ? not specified in the spec
+  | Linear
+  | MultiDimensional
+  deriving Show
+
+pDataStructureCode :: Parser DataStructureCode
+pDataStructureCode =
+  A.choice [ A.char '0' *> pure Singleton
+           , A.char '1' *> pure Linear
+           , A.char '2' *> pure MultiDimensional
+           ]
+
+
+data ArrayDescriptor
+  = ArrayDescriptor
+  deriving Show
+
+pArrayDescriptor :: Parser ArrayDescriptor
+pArrayDescriptor =
+  do A.manyTill A.anyChar ut
+     pure ArrayDescriptor
+
+
+-- 7.2.2.1 Data format
+
+data DataFormat
+ = A (Maybe Int)  -- character data
+ | I (Maybe Int)  -- implicit point representation
+ | R (Maybe Int)  -- explicit point representation
+ | B Int  -- bit string
+ | At
+ | B1W Int  -- unsigned integer
+ | B2W Int  -- signed integer
+   deriving Show
+
+pDataFormat :: Parser [DataFormat]
+pDataFormat =
+  A.choice (map rep [ do A.char 'A'
+                         mw <- pWidth
+                         pure (A mw)
+                    , do A.char 'I'
+                         mw <- pWidth
+                         pure (I mw)
+                    , do A.char 'R'
+                         mw <- pWidth
+                         pure (R mw)
+                    , do A.char 'B'
+                         w <- pWidth2
+                         pure (B w)
+                    , do A.string "b1"
+                         i <- A.digit -- should only be 1,2 or 4
+                         pure (B1W $ c2i i)
+                    , do A.string "b2"
+                         i <- A.digit -- should only be 1,2 or 4
+                         pure (B2W $ c2i i)
+                    ])
+  where
+    rep p =
+      do c <- (c2i <$> A.digit) <|> pure 1
+         r <- p
+         pure $ replicate c r
+    pWidth =
+      do A.char '('
+         i <- A.digit
+         A.char ')'
+         pure (Just $ c2i i)
+      <|> pure Nothing
+    pWidth2 =
+      do A.char '('
+         i10 <- A.digit
+         i1  <- A.digit
+         A.char ')'
+         pure $ ((c2i i10) * 10) + (c2i i1)
+
+pA :: Int -> Parser ByteString
+pA l = A.take l
+
+pI :: Int -> Parser Int
+pI l = implicit2i <$> A.count l A.digit
+
+data FormatControls = FormatControls
+  { _dataFormats :: [DataFormat]
+  }
+  deriving Show
+
+pFormatControls :: Parser FormatControls
+pFormatControls =
+  do A.char '('
+     dfs <- A.sepBy pDataFormat (A.char ',')
+     A.char ')'
+     pure (FormatControls $ concat dfs)
+
+data DataTypeCode
+  = CharacterString
+  | ImplicitPoint
+  | BinaryForm
+  | MixedDataTypes
+    deriving Show
+
+pDataTypeCode :: Parser DataTypeCode
+pDataTypeCode =
+  A.choice [ A.char '0' *> pure CharacterString
+           , A.char '1' *> pure ImplicitPoint
+           , A.char '5' *> pure BinaryForm
+           , A.char '6' *> pure MixedDataTypes
+           ]
+
+pAuxiliaryControls :: Parser ()
+pAuxiliaryControls = A.string "00" *> pure ()
+
+data FieldControls = FieldControls
+  { _dataStructureCode       :: DataStructureCode
+  , _dataTypeCode            :: DataTypeCode
+  , _truncatedEscapeSequence :: TruncatedEscapeSequence
+  }
+  deriving Show
+
+pFieldControls :: Parser FieldControls
+pFieldControls =
+  do dsc <- pDataStructureCode
+     dtc <- pDataTypeCode
+     pAuxiliaryControls
+     pPrintableGraphics
+     tes <- pTruncatedEscapeSequence
+     pure $ FieldControls dsc dtc tes
+
+
+data DataDescriptiveField = DataDescriptiveField
+  { _fieldControls    :: FieldControls
+  , _fieldName        :: ByteString
+  , _arrayDescription :: ArrayDescriptor
+  , _formatControls   :: FormatControls
+  }
+  deriving Show
+
+pDataDescriptiveField :: Parser DataDescriptiveField
+pDataDescriptiveField =
+  DataDescriptiveField <$> pFieldControls <*> pFieldName <*> pArrayDescriptor <*> pFormatControls <* ft
+
 data Module = Module
-  { _ddrLeader :: DDRLeader
-  , _ddrDirectory :: Directory
+  { _ddrLeader             :: DDRLeader
+  , _ddrDirectory          :: Directory
+  , _fieldControlField     :: FieldControlField
+  , _dataDescriptiveFields :: [DataDescriptiveField]
   }
   deriving Show
 
@@ -143,7 +346,10 @@ pModule :: Parser Module
 pModule =
   do l <- pDDRLeader
      d <- pDirectory (_entryMap l)
-     pure $ Module l d
+     fcf <- pFieldControlField
+     -- we substract 1 from the directory length because we already parsed the field control field
+     ddf <- A.count (pred (length $ _directoryEntries d)) pDataDescriptiveField
+     pure $ Module l d fcf ddf
 
 pISO8211 = pModule
 
--- Main.hs
+++ Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Data.S57.ASCII (pISO8211)
+import Data.S57.ASCII (pISO8211, Module(..), Directory(..))
 import qualified Data.ByteString.Char8 as B
 import Data.Attoparsec.ByteString.Char8 (parseOnly)
 
@@ -8,5 +8,9 @@ main :: IO ()
 main =
   do -- c <- B.readFile "ENC_ROOT/US2MI01M/US2MI01M.000"
      c <- B.readFile "ENC_ROOT/US4IL10M/US4IL10M.001"
-     print $ parseOnly pISO8211 c
+     let r = parseOnly pISO8211 c
+     print r
+     case r of
+       (Right m) -> print $ length (_directoryEntries (_ddrDirectory m))
+       _ -> pure ()
 
